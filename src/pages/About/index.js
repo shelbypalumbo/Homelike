5f17b22fdb6fc1f12a280b2270890caed33608cc
shelby palumbo <shelbypalumbo94@gmail.com>
2023-05-01 19:57:17 -0400

    updating text on about me

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./style.css";
 import ClosingDay from "../../images/AAAABackyard/Closing_Day.jpg";
+
 const About = () => {
   return (
     <div className="about">
-      <img src={ClosingDay} alt="closing day picture" className="closingDay" />
+      <img src={ClosingDay} alt={ClosingDay} className="closingDay" />
       <br></br>
       Hello! Welcome to Homelike. My name is Shelby and in 2020 I purchased my
-      very first house. After years of living in apartments, I've enjoyed the
-      process of making my house more "homelike". This website will host my
-      past/present home updates and a couple of how-to snippets.
+      first house. After years of living in apartments, I've enjoyed the process
+      of making my house more "homelike". This website will host my past/present
+      home updates and a couple of how-to snippets.
     </div>
   );
 };
